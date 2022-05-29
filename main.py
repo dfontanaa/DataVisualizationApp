050b22def8b3f04fdf8c94225090888d2a2e73c8
Danilo Fontana <danilofontanaa@gmail.com>
2022-05-29 23:56:29 +0200

    Update main.py
    Add new data investigation field

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Created on Sun May 22 19:19:23 2022
 
-@author: danil
+@author: danilo
 """
 
 import streamlit as st
@@ -12,7 +12,10 @@ from datetime import datetime
 dt = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 
 # page config
-st.set_page_config(page_title='Data Visualization App', page_icon=':bar_chart:', layout="wide")
+st.set_page_config(page_title='Data Visualization App', 
+                   page_icon=':bar_chart:',
+                   layout="wide",
+                   initial_sidebar_state='expanded')
 hide_data = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -40,7 +43,7 @@ def read_df(filepath):
 # dataframe view page
 def page_df():
     st.header('Preview DataFrame', anchor='PreviewDF')
-    datacolumns = st.multiselect('Filter data', (df.columns))
+    datacolumns = st.multiselect('Select columns to export', (df.columns))
     
     if len(datacolumns) < 1:  
         st.dataframe(df)
@@ -55,6 +58,15 @@ def page_df():
              data=csv,
              file_name='FilteredData_%s.csv'%dt,
              mime='text/csv')
+    
+    # Investigate data
+    st.write('---')
+    raw_data = st.text_input('Filter data with expression')
+    if len(raw_data) == 0:
+        pass
+    else:
+        st.write(eval(raw_data))
+    
 
 # convert file for export
 @st.cache
@@ -62,15 +74,15 @@ def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-# plot page - 5 canvas
+# plot page - 3 canvas
 def page_plot():
     st.header('Plot Data', anchor='PlotData')
     
     # canvas 1
     c1, c2, c3 = st.columns(3)
-    plt_option = c1.selectbox('Select plot option', plot_options, index=1)
-    xvalue = c2.selectbox('Select data for x axis', (df.columns))
-    yvalue = c3.multiselect('Select data for y axis', (df.columns), default = df.columns[0])
+    plt_option = c1.selectbox('Select plot option', plot_options, index=1, key='p1')
+    xvalue = c2.selectbox('Select data for x axis', (df.columns), key='p2')
+    yvalue = c3.multiselect('Select data for y axis', (df.columns), default = df.columns[0], key='p3')
     
     if len(yvalue) == 1:
         yvalue = yvalue[0]
@@ -85,6 +97,8 @@ def page_plot():
         mplt = px.line(data_frame=df, x=xvalue, y=yvalue)
     elif 'area' in plt_option:
         mplt = px.area(data_frame=df, x=xvalue, y=yvalue)
+    elif 'bar' in plt_option:
+        mplt = px.bar(data_frame=df, x=xvalue, y=yvalue)
     else:
         st.write('plot format not available yet...')
         mplt = px.scatter(data_frame=df, x=xvalue, y=yvalue)
@@ -110,121 +124,46 @@ def page_plot():
         mplt2 = px.line(data_frame=df, x=xvalue2, y=yvalue2)
     elif 'area' in plt_option2:
         mplt2 = px.area(data_frame=df, x=xvalue2, y=yvalue2)
+    elif 'bar' in plt_option2:
+        mplt2 = px.bar(data_frame=df, x=xvalue2, y=yvalue2)
     else:
         st.write('plot format not available yet...')
         mplt2 = px.scatter(data_frame=df, x=xvalue2, y=yvalue2)
     
     st.plotly_chart(mplt2, use_container_width=True)
-
-
-
-
-################################################################################################################################################
-################################################################################################################################################
-################################################################################################################################################
-
-
-
-# def plt_data():
     
-#     for i in range(3):    
-#         left, mid, right = st.columns(3)
-        
-#         with left:
-#             # chartType = st.selectbox('Select plot option', plot_options, key='l%s'%st.session_state.lid)
-#             chartType = st.selectbox('Select plot option', plot_options)
-#         with mid:
-#             # xvalue = st.selectbox('Select data for x axis', (df.columns), key='m%s'%st.session_state.mid)
-#             xvalue = st.selectbox('Select data for x axis', (df.columns))
-#         with right:
-#             # yvalue = st.multiselect('Select data for y axis', (df.columns), default = df.columns[0], key='r%s'%st.session_state.rid)
-#             yvalue = st.multiselect('Select data for y axis', (df.columns), default = df.columns[0])
-#             if len(yvalue) == 1:
-#                 yvalue = yvalue[0]
-#             elif len(yvalue) == 0:
-#                 yvalue = xvalue
-#             else:
-#                 pass
-        
-#         # add plot funct
-#         if 'scatter' in chartType:
-#             myplot = px.scatter(data_frame=df, x=xvalue, y=yvalue)
-#         else:
-#             myplot = px.scatter(data_frame=df, x=xvalue, y=yvalue)
-#         st.plotly_chart(myplot, use_container_width=True)
-        
-
-
-
-
-
-
-# def add_page_plot():
-#     st.header('Plot Data', anchor='PlotData')
-#     c1, c2, c3 = st.columns(3)
+    # canvas 3
+    c7, c8, c9 = st.columns(3)
+    plt_option3 = c7.selectbox('Select plot option', plot_options, index=1, key='p7')
+    xvalue3 = c8.selectbox('Select data for x axis', (df.columns), key='p8')
+    yvalue3 = c9.multiselect('Select data for y axis', (df.columns), default = df.columns[0], key='p9')
     
-#     plt_option = c1.selectbox('Select plot option', plot_options)
-#     xvalue = c2.selectbox('Select data for x axis', (df.columns))
-#     yvalue = c3.multiselect('Select data for y axis', (df.columns), default = df.columns[0])
+    if len(yvalue3) == 1:
+        yvalue3 = yvalue3[0]
+    elif len(yvalue3) == 0:
+        yvalue3 = xvalue3
+    else:
+        pass
     
+    if 'scatter' in plt_option3:
+        mplt3 = px.scatter(data_frame=df, x=xvalue3, y=yvalue3)
+    elif 'line' in plt_option3:
+        mplt3 = px.line(data_frame=df, x=xvalue3, y=yvalue3)
+    elif 'area' in plt_option3:
+        mplt3 = px.area(data_frame=df, x=xvalue3, y=yvalue3)
+    elif 'bar' in plt_option3:
+        mplt3 = px.bar(data_frame=df, x=xvalue3, y=yvalue3)
+    else:
+        st.write('plot format not available yet...')
+        mplt3 = px.scatter(data_frame=df, x=xvalue3, y=yvalue3)
     
-#     if len(yvalue) == 1:
-#         yvalue = yvalue[0]
-#     elif len(yvalue) == 0:
-#         yvalue = xvalue
-#     else:
-#         pass
-    
-#     if 'scatter' in plt_option:
-#         st.plotly_chart(
-#             px.scatter(
-#                 df,
-#                 x=xvalue,
-#                 y=yvalue),
-#             use_container_width=True)
-#     else:
-#        st.plotly_chart(
-#             px.scatter(
-#                 df,
-#                 x=xvalue,
-#                 y=yvalue),
-#             use_container_width=True) 
-
-
-################################################################################################################################################
-
-# def page_plot():
-#     st.header('Plot Data', anchor='PlotData')
-#     c1, c2, c3 = st.columns(3)   
-    
-#     plt_option = c1.selectbox('Select plot option', plot_options)
-#     xvalue = c2.selectbox('Select data for x axis', (df.columns))
-#     yvalue = c3.multiselect('Select data for y axis', (df.columns), default = df.columns[0])
-    
-#     if len(yvalue) == 1:
-#         yvalue = yvalue[0]
-#     elif len(yvalue) == 0:
-#         yvalue = xvalue
-#     else:
-#         pass
-    
-#     myplot = px.scatter(data_frame=df, x=xvalue, y=yvalue)
-    
-#     return myplot
+    st.plotly_chart(mplt3, use_container_width=True)
     
 
-
-
-################################################################################################################################################
-################################################################################################################################################
-################################################################################################################################################
 ################################################################################################################################################
 # Main Window
 st.title('Data Visualization App')
 
-
-
-################################################################################################################################################
 # Sidebar
 file_sel = None
 with st.sidebar:
@@ -262,23 +201,8 @@ with st.sidebar:
                 remove_canvas = n2.button('Remove Canvas')
                 
                
-                
-                    
-                    
-            
-    
-################################################################################################################################################
+# Main page        
 if file_sel is not None:
     if load_flag:
-        
         pages[selected_page]()
         
-        
-                
-            
-            
-            
-            
-            
-            
-################################################################################################################################################        
